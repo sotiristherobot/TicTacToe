b32a3da2d67e0e80a8b121698b944ee2f87d9c88
Sotiris Ioannou <me@sotiris.engineer>
2016-08-11 22:18:48 -0400

    Added arrow function to randomly select who is going to begin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+//define scope
+{
+
+  //function to randomly select who is going to begin
+  var pickUpTurn = () => {return  Math.floor((Math.random() * 2) + 1);};
+
+
+
+
+
+
+}
